1e2d87f9ff102435277edac9a2666325d9982f44
gammafn <GammaFunction@vivaldi.net>
2021-03-30 16:09:59 -0500

    Add script for importing JBot's quotes

---
+++ contrib/convert-jbot-quotes.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+jq '
+	. | with_entries( .key |= ascii_downcase) | {
+		"quotes": with_entries( .value |= .":quotes" ),
+			"aliases": to_entries | map( {(.value.":aliases"[]?): .key} ) | add
+	}
+'
