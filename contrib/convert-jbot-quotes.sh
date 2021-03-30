e6dd4503961665d483ec5e7dc1fd9599bbb4da7c
gammafn <GammaFunction@vivaldi.net>
2021-03-30 17:53:03 -0500

    Make sure to ascii_downcase the aliases

--- contrib/convert-jbot-quotes.sh
+++ contrib/convert-jbot-quotes.sh
@@ -2,6 +2,6 @@
 jq '
 	. | with_entries( .key |= ascii_downcase) | {
 		"quotes": with_entries( .value |= .":quotes" ),
-			"aliases": to_entries | map( {(.value.":aliases"[]?): .key} ) | add
+		"aliases": to_entries | map( {(.value.":aliases"[]? | ascii_downcase): .key} ) | add
 	}
 '
