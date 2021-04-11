032203a12e5738821b95c868e2f0cbc78dab0dff
gammafn <GammaFunction@vivaldi.net>
2021-04-10 22:51:59 -0500

    Allow modifying properties on the fly

--- modules/eval.py
+++ modules/eval.py
@@ -34,7 +34,7 @@ class MatrixModule(BotModule):
             body = ''
         cmd = cmd.lstrip('!')
 
-        if cmd == 'addlang':
+        if cmd in ['addlang', 'add', 'newlang', 'new']:
             bot.must_be_owner(event)
             self.logger.info(f"room: {room.name} sender: {event.sender} wants to add a language")
             args = body.split()
@@ -46,16 +46,51 @@ class MatrixModule(BotModule):
             else:
                 self.logger.info(f"room: {room.name} sender: {event.sender} is adding a language")
                 key = args[0].lower()
-                self.langmap[key] = {"container": args[1], "command": ' '.join(args[2:])}
+                self.langmap[key] = {"container": args[1], "command": args[2:]}
                 bot.save_settings()
                 await bot.send_text(room, 'Added {}'.format(args[0]))
+
+        elif cmd in ['set', 'property', 'setprop', 'setproperty']:
+            bot.must_be_owner(event)
+            self.logger.info(f"room: {room.name} sender: {event.sender} wants to modify a language")
+            args = body.split()
+
+            if len(args) < 3:
+                await bot.send_text(room, f'Usage: {cmd} [lang] [property] [value ...].')
+                return
+            lang = self.get_lang(args[0])
+            if not lang:
+                await bot.send_text(room, f'{lang} has not been added.')
+                return
+
+            # integer values
+            if args[1] in ['timeout']:
+                val = int(args[2])
+                if val <= 0:
+                    await bot.send_text(room, f'{args[1]} must be a positive integer')
+                    return
+            # string values
+            elif args[1] in ['container']:
+                val = args[2]
+            # list values
+            elif args[1] in ['podman_opts', 'command']:
+                val = args[2:]
+            # unknown values
+            else:
+                await bot.send_text(room, f'Not a property: {args[1]}')
+                return
+
+            lang[args[1]] = val
+            bot.save_settings()
+            await bot.send_text(room, f'Set property {args[1]} for {args[0]}')
+
 
         else:
             self.logger.info(f"room: {room.name} sender: {event.sender} wants to eval some code")
 
             lang, code  = self.get_code(event.formatted_body)
             # !eval [lang]
-            lang = lang or self.get_lang(cmd)
+            lang = self.get_lang(cmd) or lang
             html, plain = self.run_code(lang, code)
             await bot.send_html(room, html, plain)
 
@@ -76,9 +111,8 @@ class MatrixModule(BotModule):
         return (lang, block.contents[0].string)
 
     def get_lang(self, s):
-        for key, val in self.langmap.items():
-            if s in [key, f'language-{key}']:
-                return val
+        # Python 3.9
+        return self.langmap.get(s.removeprefix('language-'))
 
     def run_code(self, lang, code, podman_opts=[], timeout=15):
         container = lang['container']
@@ -89,7 +123,7 @@ class MatrixModule(BotModule):
         podman_opts = lang.get('podman_opts') or podman_opts
 
         proc = Popen(['podman', 'run', '--rm', '-i', '--net=none', '--workdir=/root']
-            + podman_opts + [container] + cmd.split(),
+            + podman_opts + [container] + cmd,
             stdin=PIPE, stdout=PIPE, stderr=PIPE)
         stdout, stderr = proc.communicate(input=code.encode('utf-8'), timeout=timeout)
         return ('\n'.join(i) for i in
