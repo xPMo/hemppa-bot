b13191ea89575ac1256b5fa9d85bc7651d1ca5a1
gammafn <GammaFunction@vivaldi.net>
2021-04-25 00:12:13 -0500

    Lotsa changes to !eval. Very good changes, though

--- modules/eval.py
+++ modules/eval.py
@@ -1,6 +1,10 @@
 from modules.common.module import BotModule
+from nio import RoomMessageText
+
+import shlex
 from bs4 import BeautifulSoup
 from html import escape
+
 from subprocess import run, PIPE
 
 class MatrixModule(BotModule):
@@ -12,8 +16,10 @@ class MatrixModule(BotModule):
                 **dict.fromkeys(['add', 'new', 'addlang', 'newlang'], self.add_lang),
                 **dict.fromkeys(['rm', 'remove', 'rmlang', 'delete'], self.rm_lang),
                 **dict.fromkeys(['alias', 'aliaslang'], self.alias_lang),
-                **dict.fromkeys(['set', 'setlang', 'prop', 'property'], self.set_lang_prop),
+                **dict.fromkeys(['set', 'setlang', 'setprop'], self.set_lang_prop),
                 **dict.fromkeys(['get', 'getlang', 'getprop'], self.get_lang_prop),
+                **dict.fromkeys(['list', 'ls', 'langs'], self.list_langs),
+                **dict.fromkeys(['run'], self.run_code),
         }
 
     def set_settings(self, data):
@@ -31,12 +37,19 @@ class MatrixModule(BotModule):
 
     def matrix_start(self, bot):
         super().matrix_start(bot)
-        self.add_module_aliases(bot, [*self.aliases.keys(), *self.langmap.keys()])
+        self.bot = bot
+        bot.client.add_event_callback(self.message_cb, RoomMessageText)
+        langs = [*self.aliases.keys(), *self.langmap.keys()]
+        self.add_module_aliases(bot, langs + [f'eval{key}' for key in langs])
 
-    def add_lang(self, bot, cmd, event):
+    def matrix_stop(self, bot):
+        super().matrix_stop(bot)
+        bot.remove_callback(self.message_cb)
+
+    async def add_lang(self, bot, room, event, cmd):
         bot.must_be_owner(event)
         self.logger.info(f"sender: {event.sender} wants to add a language")
-        args = event.body.split()
+        args = shlex.split(event.body)
 
         key = args[0].lower()
         if len(args) < 3:
@@ -47,9 +60,11 @@ class MatrixModule(BotModule):
 
         self.logger.info(f"sender: {event.sender} is adding a language")
         self.langmap[key] = {"container": args[1], "command": args[2:]}
-        return {'send_text': f'Added {args[0]}', 'save_settings': True}
+        self.add_module_aliases(bot, [key, f'eval{key}'])
+        bot.save_settings()
+        await bot.send_text(room, f'Added {args[0]}')
 
-    def rm_lang(self, bot, cmd, event):
+    async def rm_lang(self, bot, room, event, cmd):
         bot.must_be_owner(event)
         self.logger.info(f"sender: {event.sender} wants to remove a language")
         args = event.body.split()
@@ -57,31 +72,53 @@ class MatrixModule(BotModule):
         if len(args) != 1:
             return {'send_text': f'{cmd} takes exactly one arguments'}
 
-        self.langmap.pop(args[0])
-        return {'send_text': f'removed {args[0]}', 'save_settings': True}
+        try:
+            self.langmap.pop(args[0])
+            self.aliases = {k:v for k, v in self.aliases.items() if v != args[0]}
+            bot.save_settings()
+            await bot.send_text(room, f'removed language {args[0]}')
+            return
+        except KeyError:
+            pass
+        try:
+            self.aliases.pop(args[0])
+            bot.save_settings()
+            await bot.send_text(room, f'removed alias {args[0]}')
+        except:
+            await bot.send_text(room, f'No language or alias found')
 
-    def alias_lang(self, bot, cmd, event):
+    async def list_langs(self, bot, room, event, cmd):
+        ret = []
+        for name in self.langmap.keys():
+            aliases = [k for k, v in self.aliases.items() if v == name]
+            if aliases:
+                name += ' ({})'.format(' '.join(aliases))
+            ret.append(name)
+        await bot.send_text(room, '\n'.join(ret))
+
+    async def alias_lang(self, bot, room, event, cmd):
         bot.must_be_owner(event)
         self.logger.info(f"sender: {event.sender} wants to alias a language")
         args = event.body.split()
 
         if len(args) < 2:
-            return {'send_text': f'{cmd} takes two arguments'}
+            msg = f'{cmd} takes two arguments'
 
         args = [arg.lower() for arg in args]
         if args[0] in self.langmap.keys():
-            return {'send_text': f'Already a language: {args[0]}'}
+            msg = f'Already a language: {args[0]}'
 
         if not args[1] in self.langmap.keys():
-            return {'send_text': f'Not a language: {args[1]}'}
+            msg = f'Not a language: {args[1]}'
 
         self.aliases[args[0]] = args[1]
+        self.add_module_aliases(bot, [args[0], f'eval{args[0]}'])
         return {'send_text': f'Added {args[0]}', 'save_settings': True}
 
-    def set_lang_prop(self, bot, cmd, event):
+    async def set_lang_prop(self, bot, room, event, cmd):
         bot.must_be_owner(event)
         self.logger.info(f"sender: {event.sender} wants to modify a language")
-        args = event.body.split()
+        args = shlex.split(event.body)
 
         if len(args) < 3:
             return {'send_text': f'Usage: {cmd} [lang] [property] [value ...].'}
@@ -107,20 +144,19 @@ class MatrixModule(BotModule):
         lang[args[1]] = val
         return {'send_text': f'Set property {args[1]} for {args[0]}', 'save_settings': True}
 
-    def get_lang_prop(self, bot, cmd, event):
+    async def get_lang_prop(self, bot, room, event, cmd):
         self.logger.info(f"sender: {event.sender} wants to list a language's properties")
-        args = event.body.split()
-
-        lang = self.get_lang(args[0])
+        lang = self.get_lang(event.body.split(None, 1)[0])
         if not lang:
-            return {'send_text': f'{lang} has not been added.'}
+            msg = f'{lang} has not been added.'
+        else:
+            msg = [f'{args[0]}:']
+            for key, val in lang.items():
+                msg.append(f'- {key}: {val}')
+            msg = '\n'.join(msg)
+        await bot.send_text(room, msg)
 
-        ret = [f'{args[0]}:']
-        for key, val in lang.items():
-            ret.append(f'- {key}: {val}')
-        return {'send_text': '\n'.join(ret)}
-
-    def run_code(self, bot, cmd, event):
+    async def run_code(self, bot, room, event, cmd):
         self.logger.info(f"sender: {event.sender} wants to eval some code")
         lang, code = self.get_code(cmd, event)
         container = lang['container']
@@ -140,12 +176,36 @@ class MatrixModule(BotModule):
 
         proc = run(['podman', 'run', '--rm', '-i'] + podman_opts + [container] + podman_cmd,
                 input=code.encode('utf-8'), stdout=PIPE, stderr=PIPE, timeout=timeout)
-        parts = [self.code_block('stdout', proc.stdout.decode().strip()), self.code_block('stderr', proc.stderr.decode().strip())]
+        parts = [self.code_block('stdout', proc.stdout.decode().strip('\n')), self.code_block('stderr', proc.stderr.decode().strip('\n'))]
         if proc.returncode != 0:
             parts.insert(0, (f'<p><strong>Process exited non-zero</strong>: <code>{proc.returncode}</code></p>',
                     f'(Process exited non-zero: {proc.returncode})'))
 
-        return {'send_html': ('\n'.join(i) for i in zip(*parts))}
+        html, plain = ('\n'.join(i) for i in zip(*parts))
+        await bot.send_html(room, html, plain)
+
+    async def message_cb(self, room, event):
+        """
+        Handle client callbacks for all room text events
+        """
+        if self.bot.should_ignore_event(event):
+            return
+
+        # no content at all?
+        if len(event.body) < 1:
+            return
+
+        if "content" in event.source:
+            # skip edited content to prevent spamming the same thing multiple times
+            if "m.new_content" in event.source["content"]:
+                self.logger.debug("Skipping edited event to prevent spam")
+                return
+            # skip reply messages to prevent spam
+            if "m.relates_to" in event.source["content"]:
+                self.logger.debug("Skipping reply message to prevent spam")
+                return
+        # TODO
+        return
 
     async def matrix_message(self, bot, room, event):
         try:
@@ -159,14 +219,7 @@ class MatrixModule(BotModule):
         cmd = cmd.lstrip('!')
 
         op = self.commands.get(cmd) or self.run_code
-        for key, val in op(bot, cmd, event).items():
-            if key == 'send_text':
-                await bot.send_text(room, val)
-            elif key == 'send_html':
-                html, plain = val
-                await bot.send_html(room, html, plain)
-            elif key == 'save_settings' and val:
-                bot.save_settings()
+        await op(bot, room, event, cmd)
 
     def get_code(self, cmd, event):
         lang = None
@@ -204,5 +257,28 @@ class MatrixModule(BotModule):
             return (f'<p><em>no {escape(header)}</em>', f'(no {header})')
 
     def help(self):
-        return 'Evaluate code blocks or !addlang [image] [cmd ...]'
+        return 'Evaluate code in a container'
 
+
+    def long_help(self, bot=None, event=None, **kwargs):
+        text = self.help() + (
+                '\n- !eval (list|ls|langs): list the current languages and their aliases'
+                '\n- !eval (get|getprop) [lang]: get the current properties for the given language'
+                '\n- !eval run [lang] [code]: run code (see below)'
+                '\n- !eval[lang] [code]: run code (see below)'
+                '\n- ![lang] [code]: run code (see below)'
+                )
+        if bot and event and bot.is_owner(event):
+            text += ('\n- !eval (add|new) [lang] [container] [command ...]: add an new language'
+                     '\n- !eval alias [name] [lang]: add an alias [name] for [lang]'
+                     '\n- !eval (set|setprop) [lang] [property] [value]: alter a language'
+                     '\n- !eval (remove|rm) [name]: remove a language, or alias to a language'
+                     )
+        text += ('\nRunning code:'
+                 '\n    !eval will attempt to find a code block in the message\'s formatteed_body.'
+                 '\n    If a multiline codeblock has a language specifier, that language is preferred.'
+                 '\n    Otherwise, the first codeblock (inline or otherwise) is used'
+                 'with the language given in one of the forms above.'
+                 '\n    If no codeblock is found, then the remaining body after [lang] is interpreted as code.'
+                 )
+        return text
