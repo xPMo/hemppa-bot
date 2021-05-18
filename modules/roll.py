535b0d3d74681637dca707d88f8c11f3fc89610a
gammafn <GammaFunction@vivaldi.net>
2021-05-18 01:02:25 -0500

    Add +/- bonus spec to dice

--- modules/roll.py
+++ modules/roll.py
@@ -4,20 +4,30 @@ from random import randrange
 class MatrixModule(BotModule):
 
     async def matrix_message(self, bot, room, event):
-        self.logger.info(f"room: {room.name} sender: {event.sender} is rolling dice")
+        self.logger.info(f"room: {room.name}; sender: {event.sender} is rolling dice")
         args = event.body.split()[1:]
         if not args:
-            return await bot.send_text(room, 'Missing argument')
+            return await bot.send_text(room, 'Missing dice argument')
         try:
             res = []
+            bonus = 0
             for arg in args:
+                if arg[0] in ['+', '-']:
+                    bonus += int(arg)
+                    continue
                 count, sides = arg.split('d')
                 sides = int(sides)
                 count = int(count or 1)
                 res += [1 + randrange(sides) for _ in range(count)]
-            if len(res) < 2:
+            if len(res) < 2 and not bonus:
                 return await bot.send_text(room, str(res[0]))
-            return await bot.send_text(room, f'{"+".join(map(str, res))} = {sum(res)}')
+            rhs = sum(res) + bonus
+            lhs = '+'.join(map(str, res))
+            if bonus > 0:
+                lhs += f'+{bonus}'
+            elif bonus < 0:
+                lhs += f'{bonus}'
+            return await bot.send_text(room, f'{lhs} = {rhs}')
         except ValueError:
             return await bot.send_text(room, f'Invalid dice spec: {arg}. Use the form [count]d[sides]')
 
