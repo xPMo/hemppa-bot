e8149a3c5498b566e77ee508c686a3f7e981b982
gammafn <GammaFunction@vivaldi.net>
2022-01-16 20:59:32 -0600

    put except in a more obvious spot

--- modules/roll.py
+++ modules/roll.py
@@ -8,9 +8,9 @@ class MatrixModule(BotModule):
         args = event.body.split()[1:]
         if not args:
             return await bot.send_text(room, 'Missing dice argument')
+        res = []
+        bonus = 0
         try:
-            res = []
-            bonus = 0
             for arg in args:
                 if arg[0] in ['+', '-']:
                     bonus += int(arg)
@@ -19,17 +19,20 @@ class MatrixModule(BotModule):
                 sides = int(sides)
                 count = int(count or 1)
                 res += [1 + randrange(sides) for _ in range(count)]
-            if len(res) < 2 and not bonus:
-                return await bot.send_text(room, str(res[0]))
-            rhs = sum(res) + bonus
-            lhs = '+'.join(map(str, res))
-            if bonus > 0:
-                lhs += f'+{bonus}'
-            elif bonus < 0:
-                lhs += f'{bonus}'
-            return await bot.send_text(room, f'{lhs} = {rhs}')
         except ValueError:
             return await bot.send_text(room, f'Invalid dice spec: {arg}. Use the form [count]d[sides]')
+
+        # only one die
+        if len(res) < 2 and not bonus:
+            return await bot.send_text(room, str(res[0]))
+
+        rhs = sum(res) + bonus
+        lhs = '+'.join(map(str, res))
+        if bonus > 0:
+            lhs += f'+{bonus}'
+        elif bonus < 0:
+            lhs += f'{bonus}'
+        return await bot.send_text(room, f'{lhs} = {rhs}')
 
     def help(self):
         return 'Roll [count]d[sides] ... dice (e.g.: !roll 3d6 d20)'
